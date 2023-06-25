5e5eb44ab2eccd945711da1372cd47493a2d6a69
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-26 04:57:42 +0500

    Question 1

--- Assignment 2/Q_1_.dart
+++ Assignment 2/Q_1_.dart
@@ -1,3 +1,9 @@
 void main(){
+
+  // Create a list of names and print all names using list
+
+  List<String?> names = ['Ahmed','Asim','Usman','Babu','Naveed','Hamza','Awais',];
+
+  print(names);
   
 }
