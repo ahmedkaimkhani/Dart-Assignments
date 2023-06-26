f52cbf0094763952f5a81bade86e5290748aa1e9
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-27 01:48:40 +0500

    Question 7

--- Assignment 2/Q.6.dart
+++ Assignment 2/Q.6.dart
@@ -1,4 +1,9 @@
 void main() {
+
+  // Q.6: Create map variable name world then inside it create countries map.
+  // key will be the name country & country value will have another map having CapitalCity,
+  // currency and language to it. by using any country key print all the value of Capital & Currency.
+  
   // Create the world map
   Map<String, Map<String, String>> world = {
     'Pakistan': {
--- Assignment 2/Q.7.dart
+++ Assignment 2/Q.7.dart
@@ -1,9 +1,21 @@
+void main (){
 
+//   Q.7:
+// Map<String, double> expenses = {
+//   'sun': 3000.0,
+//   'mon': 3000.0,
+//   'tue': 3234.0,
+// };
 
+// Check if "fri" exist in expanses; if exist change it's value to 5000.0 
+//otherwise add 'fri' to expenses and set its value to 5000.0 then print expenses.
 
-
-
-
-  // Q.6: Create map variable name world then inside it create countries map.
-  // key will be the name country & country value will have another map having CapitalCity,
-  // currency and language to it. by using any country key print all the value of Capital & Currency.
+Map<String, double> expenses = {
+  'sun': 3000.0,
+  'mon': 3000.0,
+  'tue': 3234.0,
+};
+// we can change value of key and also add value,
+expenses.addAll({'mon': 4000.0,'fri': 5000.0});
+print(expenses);
+}
