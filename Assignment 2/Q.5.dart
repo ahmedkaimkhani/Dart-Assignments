8a8f2b08de9c920d9ea6d60130d771a207164319
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-26 20:48:50 +0500

    Question 5

--- Assignment 2/Q.5.dart
+++ Assignment 2/Q.5.dart
@@ -3,5 +3,13 @@ void main(){
   // Q.5: Create a map with name, phone keys and store some
   // value to it. Use where to find all keys that have lenght 4.
 
-  
+  Map<String, dynamic> userData = {
+    'name': 'Ahmed',
+    'email': 'ak9302359@gmail.com',
+    'contact': '03155642789',
+    'address': 'Karachi'
+  };
+  Map mapLenght = userData..keys.where((key) => key.length == 4).toList();
+
+  print('key with lenght 4 $mapLenght');
 }
