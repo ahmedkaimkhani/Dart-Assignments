acd5a774769d387d32151f775d046bdc0cd81ec6
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-26 05:12:53 +0500

    Question 2

---
+++ Assignment 2/Q.1.dart
@@ -0,0 +1,18 @@
+void main (){
+
+  // Q.1: Create an empty list of type string called days.
+  // Use the add method to add names of 7 days and print all days.
+
+  List<String?> days = [];
+
+  days.addAll([
+    'Monday',
+     'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday',
+      ]);
+      print(days);
+}
--- Assignment 2/Q_1_.dart
+++ Assignment 2/Q_1_.dart
@@ -1,6 +1,6 @@
 void main(){
 
-  // Create a list of names and print all names using list
+  // Q.2: Create a list of names and print all names using list
 
   List<String?> names = ['Ahmed','Asim','Usman','Babu','Naveed','Hamza','Awais',];
 
