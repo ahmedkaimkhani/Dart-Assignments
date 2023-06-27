b55170462cce5945b51c10576367f33e0648b687
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-28 01:05:20 +0500

    Qestion 17

--- Assignment 2/Q.17.dart
+++ Assignment 2/Q.17.dart
@@ -3,5 +3,13 @@ void main (){
   // Q.17: Given a list of integers, write a Dart function named squareValues that
   // uses the map() method to create a new list with each value squared. The function
   // should take in the original list as a parameter and return the new list.
+  
+  List<int> originalList = [1,2,3,4,5,6];
+
+  List<int> squareList = originalList.map((number) => number * number).toList();
+
+  print('Original List $originalList');
+
+  print('Square List $squareList');
 
 }
--- Assignment 2/Q.18.dart
+++ Assignment 2/Q.18.dart
@@ -4,14 +4,15 @@ void main (){
   // "age" as 25, "isStudent" as true. Write a Dart code to check if the person is both a student
   // and over 18 years old. Print "Eligible" if both conditions are true, otherwise print "Not eligible".
 
+  // create a map of student name 
   Map<dynamic, dynamic > students = {
     'name': 'Ahmed',
     'age': 18,
     'isStudent': true, 
   };
 
+  // create if else method to give condition and check student is eligible or not.
   if (students['age'] >= 18 && students['isStudent'] == true){
-    
     print('You are Eligible');
   }else {
     print('Yor are not Eligible');
