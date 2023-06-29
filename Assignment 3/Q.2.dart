5f3d77dc55047378624c5a2a992f681b33e2ece5
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-29 17:27:06 +0500

    Question 2

---
+++ Assignment 3/Q.2.dart
@@ -0,0 +1,13 @@
+void main (){
+
+  // Q.2: Write a program that prints the Fibonacci sequence up to a given
+ // number using a for loop.
+ // Example:
+ // Input: 10
+ // Output: 0 1 1 2 3 5 8
+ 
+ int input = 10;
+ for (int i = 0; i < input; i++){
+  print(input);
+ }
+}
