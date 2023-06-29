8f011343f381e74b47e8597aa9bb92322db2042d
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-29 21:10:43 +0500

    Question 2

--- Assignment 3/Q.2.dart
+++ Assignment 3/Q.2.dart
@@ -6,8 +6,42 @@ void main (){
  // Input: 10
  // Output: 0 1 1 2 3 5 8
  
- int input = 10;
- for (int i = 0; i < input; i++){
-  print(input);
+ int input = 10; // The number of Fibonacci sequence elements to print
+
+ int a = 0; // First Fibonacci number
+ int b = 1; // Second Fibonacci number
+ 
+ print(a); // Print the first Fibonacci number
+ print(b); // Print the second Fibonacci number
+
+ // Starting from i = 2 since the first two Fibonacci numbers are already printed
+ for (int i = 2; i < 7; i++){
+  if (i >= 1){
+    int c = a + b; // Calculate the next Fibonacci number by adding the previous two numbers
+    a = b;
+    b = c;
+    print(c);
+  }
+  
  }
+
+//  int n = 10; // The number of Fibonacci sequence elements to print
+
+//   int a = 0; // First Fibonacci number
+//   int b = 1; // Second Fibonacci number
+
+//   print(a); // Print the first Fibonacci number
+//   print(b); // Print the second Fibonacci number
+
+//   for (int i = 2; i < 7; i++) {
+//     // Starting from i = 2 since the first two Fibonacci numbers are already printed
+
+//     int c = a + b;
+//     // Calculate the next Fibonacci number by adding the previous two numbers
+
+//     print(c); // Print the next Fibonacci number
+
+//     a = b; // Update a with the value of b for the next iteration
+//     b = c; // Update b with the value of c for the next iteration
+//   }
 }
