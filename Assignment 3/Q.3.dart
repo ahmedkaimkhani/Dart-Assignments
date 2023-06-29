54f8ee08bc919ab6c3129aacad0fee4eedb36701
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-29 23:58:21 +0500

    Question 4

---
+++ Assignment 3/Q.3.dart
@@ -0,0 +1,15 @@
+void main (){
+
+  // Q.3: Implement a code that checks whether a given number is prime or not.
+ // Example:
+ // Input: 17
+ // Output: 17 is a prime number.
+ 
+ for (int i = 2; i <= 10 / i; ){
+  if (10 % i == 0){
+   // print('Number is not Prime');
+  } else {
+   // print('Number is Prime');
+  }
+ }
+}
