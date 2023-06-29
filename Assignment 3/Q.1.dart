f3be125b20ab09ca7850c1471af520718720bd9e
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-29 17:03:51 +0500

    Question 1

--- Assignment 3/Q.1.dart
+++ Assignment 3/Q.1.dart
@@ -4,14 +4,36 @@ void main() {
 // Example:
 // Input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // Output: 2 4 6 8 10
-
+  
+  // Note: i do with for even and odd both.
+  // create a list of integers and empty list of even and odd both for adding in these list.
   List numbers = [1,2,3,4,5,6,7,8,9];
   List evenNumbers = [];
   List oddNumbers = [];
+  
+  // For in Loop Method
+  
+  // for (int i in numbers){
+  //   if (i % 2 == 0){
+  //    evenNumbers.add(i);
+  //    print(evenNumbers);
+  //   } else {
+  //     oddNumbers.add(i);
+  //     print(oddNumbers);
+  //   }
+  // }
 
-  for (int i in numbers){
+  // For range Loop Method
+  for (int i = 1; i <= numbers.length; i++){
+
+    // using if else condition for seprating numbers into even and odd
     if (i % 2 == 0){
-      print(evenNumbers[i]);
+      evenNumbers.add(i);
+    }else {
+      oddNumbers.add(i);
     }
   }
+  /// print event numbers & odd numbers
+  print('Even Numbers $evenNumbers');
+  print('Odd Numbers $oddNumbers');
 }
