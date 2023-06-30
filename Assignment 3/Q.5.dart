fe7bff5090f8bc42fdf64884175de7d937c5cfb8
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-01 02:06:38 +0500

    Question 6

--- Assignment 3/Q.5.dart
+++ Assignment 3/Q.5.dart
@@ -5,11 +5,15 @@ void main (){
 // Input: 12345
 // Output: Sum of digits: 15
 
-
+// intialize the value of how many times loop run.
 int number = 5;
+// initialize sum 0 to store 1 to 5 digits sum.
 int sum = 0;
+
+// initialize i for itration.
 int i = 1;
 
+// while loop method to do sum of all digits
 while (i <= number){
   sum += i;  // Equivalent to: sum = sum + i;
   i++;
--- Assignment 3/Q.6.dart
+++ Assignment 3/Q.6.dart
@@ -1,3 +1,14 @@
 void main (){
-  
+//  Q.6: Implement a code that finds the largest element in a list using a for
+// loop.
+// Example:
+// Input: [3, 9, 1, 6, 4, 2, 8, 5, 7]
+// Output: Largest element: 9
+
+List numbers = [3, 9, 1, 6, 4, 2, 8, 5, 7];
+
+for (int i = 0; i < numbers.length; i++){
+  numbers.sort();
+}
+print('Largest element: ${numbers.last}');
 }
