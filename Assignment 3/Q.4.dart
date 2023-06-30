c61889bd5c6571f0f12da1d329b1fbb32854f692
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-01 00:41:37 +0500

    Question 4

--- Assignment 3/Q.4.dart
+++ Assignment 3/Q.4.dart
@@ -6,11 +6,15 @@ void main (){
  // Input: 5
  // Output: Factorial of 5 is 120
  
+ // initialize that number you want factorial of that number
  int number = 5;
- int factorial = 1;
 
+ // for multiply
+ int factorial = 1;
+ // for loop method to check factorial
  for (int i = 1; i <= number; i++){
-  factorial = factorial * i;
+  factorial *= i; // Equivalent to: factorial = factorial * i;
+  // print('Factorial of $number in $factorial');
  }
  print('Factorial of $number in $factorial');
  }
