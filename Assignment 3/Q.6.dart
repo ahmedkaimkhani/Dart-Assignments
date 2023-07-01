59bab77d19bb935bf8f1d5a08284efbbbc9ec7fb
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-01 15:37:33 +0500

    Question 7

--- Assignment 3/Q.6.dart
+++ Assignment 3/Q.6.dart
@@ -5,10 +5,14 @@ void main (){
 // Input: [3, 9, 1, 6, 4, 2, 8, 5, 7]
 // Output: Largest element: 9
 
+// iniialize list of integers 
 List numbers = [3, 9, 1, 6, 4, 2, 8, 5, 7];
 
+// using for loop method
 for (int i = 0; i < numbers.length; i++){
+  // sorting list numbers to get largest number
   numbers.sort();
 }
+// print largest element using numbers.last
 print('Largest element: ${numbers.last}');
 }
