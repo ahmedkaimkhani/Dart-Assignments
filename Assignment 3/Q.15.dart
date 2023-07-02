e0cfe5f88dc6aeefac5c757dffeea5ef75b9d142
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-03 02:02:22 +0500

    Question 15

---
+++ Assignment 3/Q.15.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+
+void main() {
+  int numberOfRows = 4;
+  int number = 1;
+
+  for (int i = 1; i <= numberOfRows; i++) {
+    for (int j = 1; j <= numberOfRows - i; j++) {
+      stdout.write(" ");
+    }
+    for (int k = 1; k <= i; k++) {
+      stdout.write("$number ");
+      number++;
+    }
+    stdout.writeln(""); // Print a new line after each row
+  }
+}
