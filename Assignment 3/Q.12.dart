fa86e3e942c561a3f7a4e6b359ea921fecbcbd6c
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-03 00:52:55 +0500

    Question 13

--- Assignment 3/Q.12.dart
+++ Assignment 3/Q.12.dart
@@ -12,9 +12,15 @@ void main (){
 int numbersOfTimes = 4;
 
 for (int i = 1; i <= numbersOfTimes; i++){
+
+  // the variable j is used as a loop counter in the inner loop.
+  // It represents the number of asterisks to be printed in each row.
   for (int j = 1; j <= i; j++){
+
+    // print statement give you newline when printing again.
+      // while "stdout.write" doesn't give you the newline. it continou to print on the same line.
     stdout.write(j);
   }
-  print('');
+  print(''); // Print a new line after each row
 }
 }
