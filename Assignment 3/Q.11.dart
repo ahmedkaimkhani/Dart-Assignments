3e9b5a99bfc8e4aea746c63d1df73f1104b8efe4
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-03 00:34:49 +0500

    Question 11

--- Assignment 3/Q.11.dart
+++ Assignment 3/Q.11.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main (){
  // Q.11: Write a program to display a pattern like a right angle triangle using an
  // asterisk using loop.
@@ -8,10 +10,10 @@ void main (){
  // ****
  int numbersOfTimes = 4;
 
- for (int i = 1; i <= numbersOfTimes; i++){
-  for (int j = 1; j <= i; j++){
-    print('*');
+ for (int i = 1; i <= numbersOfTimes; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write("*");
+    }
+    print(""); // Print a new line after each row
   }
-  print('');
- }
 }
