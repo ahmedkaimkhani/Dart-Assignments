e9ca93d1758975ae206e9f6ce9596ebe2801df8d
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-03 00:50:36 +0500

    Question 12

--- Assignment 3/Q.12.dart
+++ Assignment 3/Q.12.dart
@@ -1,3 +1,20 @@
+import 'dart:io';
+
 void main (){
-  
+//   Q.12: Write a program to display a pattern like a right angle triangle with a
+// number using loop.
+// The pattern like :
+// 1
+// 12
+// 123
+// 1234
+
+int numbersOfTimes = 4;
+
+for (int i = 1; i <= numbersOfTimes; i++){
+  for (int j = 1; j <= i; j++){
+    stdout.write(j);
+  }
+  print('');
+}
 }
