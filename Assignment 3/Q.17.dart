6fee7b6c5ed7d9ebeaa23d872322b189898d07c6
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-04 03:21:43 +0500

    Question 17

--- Assignment 3/Q.17.dart
+++ Assignment 3/Q.17.dart
@@ -6,14 +6,30 @@ void main (){
   // login successful." Otherwise, keep asking for the email and password
   // until the correct credentials are provided.
 
-  String email = "ak9302359@gmail.com";
-  String password = 'ahmed59061@';
-  bool login = false;
-  String a = 'Enter your email';
-  String b = 'Enter your password';
+  // stdout.write('Enter your email');
 
-  stdout.write(a);
+  print('Enter first number:');
+  int numberOne = int.parse(stdin.readLineSync()!);
 
-  
+  stdout.write('Enter second number:');
+  int numberTwo = int.parse(stdin.readLineSync()!);
+
+  if (numberOne % 2 == 0) {
+    print('$numberOne is an even number');
+  } else {
+    print('$numberOne is an odd number');
+  }
+
+  if (numberTwo % 2 == 0) {
+    print('$numberTwo is an even number');
+  } else {
+    print('$numberTwo is an odd number');
+  }
+  if (numberOne == numberTwo) {
+    print('$numberOne is equal to $numberTwo');
+  }
+  if (numberOne + numberTwo == 10) {
+    print('numbers are equal');
+  }
   
 }
