fb41da7f6fec83e59ec26e531ac42a0a7280de1a
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-04 03:04:21 +0500

    Question 17

---
+++ Assignment 3/Q.17.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main (){
+  // Q.17: Write a program that asks the user for their email and password. If the
+  // email and password match a predefined set of credentials, print "User
+  // login successful." Otherwise, keep asking for the email and password
+  // until the correct credentials are provided.
+
+  String email = "ak9302359@gmail.com";
+  String password = 'ahmed59061@';
+  bool login = false;
+  String a = 'Enter your email';
+  String b = 'Enter your password';
+
+  stdout.write(a);
+
+  
+  
+}
