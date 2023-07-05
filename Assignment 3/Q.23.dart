04c3bff763db56356e769fb7532573ec4ce56773
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-06 00:33:06 +0500

    Question 18

---
+++ Assignment 3/Q.23.dart
@@ -0,0 +1,8 @@
+void main (){
+  // Q.23: Write a program that takes a list of student details as input, where
+  // each student is represented by a map containing their name, marks,
+  // section, and roll number. The program should determine the grade of
+  // each student based on their average score (assuming maximum marks
+  // for each subject is 100) and print the student's name along with their
+  // grade.
+}
